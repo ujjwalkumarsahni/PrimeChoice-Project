8cd0b44adcf0d58c6868ca3874c466ee477ee99b
ujjwalkumarsahni <ujjwalkumar0514@gmail.com>
2025-08-19 10:12:28 -0700

    connect database, cloudinary and create model for product and user

--- server/server.js
+++ server/server.js
@@ -1,17 +1,34 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongoDB.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // app config
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-// API Endpoints
-app.get('/' , (req,res) => res.send("API Working fine"))
+// routes
+app.get("/", (req, res) => res.send("API Working fine"));
 
+// start server
+const startServer = async () => {
+  try {
+    // connect to database
+    await connectDB();
+    connectCloudinary()
 
-app.listen(port , () => console.log('Server running on ' + port))
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+    process.exit(1); 
+  }
+};
+
+startServer();
