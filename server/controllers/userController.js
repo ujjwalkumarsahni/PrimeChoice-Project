6d1790662fef68f72d3e9b140014ef0807e0a0c1
ujjwalkumarsahni <ujjwalkumar0514@gmail.com>
2025-08-19 16:49:42 -0700

    admin login functionality implement

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -81,7 +81,13 @@ export const registerUser = async (req, res) => {
 // -------------------- Admin Login --------------------
 export const adminLogin = async (req, res) => {
   try {
-
+    const {email,password} = req.body
+    if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+      const token = jwt.sign(email+password,process.env.JWT_SECRET)
+      res.json({success: true, token})
+    } else{
+      res.json({success: false, message: "Invalid Credentials"})
+    }
   } catch (error) {
     console.error("Admin login error:", error);
     res.status(500).json({ success: false, message: "Server error" });
