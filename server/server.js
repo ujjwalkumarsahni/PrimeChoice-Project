62b6d2c44e54784c5bb7ff99f387afcb238f6036
ujjwalkumarsahni <ujjwalkumar0514@gmail.com>
2025-08-19 23:59:19 -0700

    controller for add to cart, update cart and getcart data

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongoDB.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 // app config
 const app = express();
@@ -17,6 +18,7 @@ app.use(express.json());
 // routes endpoint
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
 app.get("/", (req, res) => res.send("API Working fine"));
 
 // start server
