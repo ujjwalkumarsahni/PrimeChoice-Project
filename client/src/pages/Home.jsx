3e167afef5c9d447d22c2078ee11a9a6dc1d9794
ujjwalkumarsahni <ujjwalkumar0514@gmail.com>
2025-08-21 11:55:09 -0700

    fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,7 @@ import Hero from '../components/Hero.jsx'
 import LatestCollection from '../components/LatestCollection.jsx'
 import BestSeller from '../components/BestSeller.jsx'
 import OurPolicy from '../components/OurPolicy.jsx'
-import NewsletterBox from '../components/Newsletterbox.jsx'
-
+import NewsletterBox from '../components/NewsletterBox.jsx'
 const Home = () => {
   return (
     <div>
