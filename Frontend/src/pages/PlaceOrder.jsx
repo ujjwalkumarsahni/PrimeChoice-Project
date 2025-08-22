67d9ad33c6e08b5eda7724b5fa59e9c7cb218794
ujjwalkumarsahni <ujjwalkumar0514@gmail.com>
2025-08-22 07:51:08 -0700

    fix code

--- Frontend/src/pages/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder.jsx
@@ -96,6 +96,7 @@ const PlaceOrder = () => {
           const response = await axios.post(`${backendUrl}/api/order/place`, orderData, { headers: { token } })
           if (response.data.success) {
             setCartItems({})
+            toast.success(response.data.message)
             navigate('/orders')
           } else {
             toast.error(response.data.message)
